46fb8bf93e98354b8ef29a81167dd4cfaaaf90ab
Ching Pei Yang <badnam3o.0@gmail.com>
2022-11-21 03:00:23 +0100

    tweaks

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -16,12 +16,11 @@ configs.setup({
   
   highlight = {
 		enable = true, -- false will disable the whole extension
-		disable = { "css" }, -- list of language that will be disabled
 	},
 	autopairs = {
 		enable = true,
 	},
-	indent = { enable = true, disable = { "python", "css" } },
+	indent = { enable = true },
 
 	context_commentstring = {
 		enable = true,
