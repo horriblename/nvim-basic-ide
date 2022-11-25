9d7bfbb20de4e19b5a3e051dc530d92c70feb9db
Ching Pei Yang <badnam3o.0@gmail.com>
2022-11-25 02:30:23 +0100

    add util function get_HUD_filetypes

--- lua/user/util.lua
+++ lua/user/util.lua
@@ -5,7 +5,21 @@ local M = {}
 -- @tparam string sessionfile
 -- @treturn string the location of the session
 function M.persistence_loc(session_file)
-  return vim.fn.fnamemodify(session_file, ':t:r:gs|%|/|:~')
+	return vim.fn.fnamemodify(session_file, ":t:r:gs|%|/|:~")
+end
+
+function M.get_HUD_filetypes()
+	return {
+		"lspinfo",
+		"packer",
+		"checkhealth",
+		"help",
+		"man",
+		"mason",
+		"netrw",
+		"NvimTree",
+		"",
+	}
 end
 
 return M
