f398d5579a75a24a421f4fe6cae7b49d762817b2
Ching Pei Yang <badnam3o.0@gmail.com>
2022-11-25 02:28:40 +0100

    tweaks

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,5 +1,11 @@
+vim.wo.colorcolumn = nil
+vim.bo.shiftwidth = 4
+vim.bo.tabstop = 4
+vim.bo.softtabstop = 4
+vim.bo.expandtab = true
+
 -- this links to tokyonight's highlight definitions for the "p00f/nvim-ts-rainbow" plugin
-vim.cmd [[ 
+vim.cmd([[ 
 hi! link @Header1 rainbowcol1
 hi! link @Header2 rainbowcol2
 hi! link @Header3 rainbowcol3
@@ -7,9 +13,7 @@ hi! link @Header4 rainbowcol4
 hi! link @Header5 rainbowcol5
 hi! link @Header6 rainbowcol6
 
-hi @text.emphasis guifg=#e0af68	" rainbowcol2
-hi @text.strong guifg=#9ece6a " rainbowcol3
-hi @text.stronger guifg=#1abc9c
-]]
-
-vim.wo.colorcolumn = nil
+hi @text.emphasis guifg=#e0af68 gui=italic  " rainbowcol2
+hi @text.strong guifg=#9ece6a gui=bold  " rainbowcol3
+hi @text.stronger guifg=#1abc9c gui=bold,italic
+]])
