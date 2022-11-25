6a672d9e450627fbc4f9f1484bd73e7521d74167
Ching Pei Yang <badnam3o.0@gmail.com>
2022-11-25 02:29:27 +0100

    (experimental)add nvide path to sumneko lua's lib path

--- lua/user/lsp/settings/sumneko_lua.lua
+++ lua/user/lsp/settings/sumneko_lua.lua
@@ -8,6 +8,9 @@ return {
         library = {
           [vim.fn.expand "$VIMRUNTIME/lua"] = true,
           [vim.fn.stdpath "config" .. "/lua"] = true,
+          -- these are not working:
+          [require 'user.nvide-path':get_nvide_config_dir() .. '/lua'] = true,
+          [require 'user.nvide-path':get_nvide_data_dir() .. '/site'] = true,
         },
       },
       telemetry = {
