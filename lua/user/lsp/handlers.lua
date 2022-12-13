1050d32ba806334f12561689afa15b348e383412
Ching Pei Yang <badnam3o.0@gmail.com>
2022-12-13 02:37:08 +0100

    expose lsp_keymaps in case it doesn't setup properly

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -51,7 +51,7 @@ M.setup = function()
 	})
 end
 
-local function lsp_keymaps(bufnr)
+function M.lsp_keymaps(bufnr)
 	local opts = { noremap = true, silent = true }
 	local keymap = vim.api.nvim_buf_set_keymap
 	keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
@@ -59,7 +59,7 @@ local function lsp_keymaps(bufnr)
 	keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
 	keymap(bufnr, "n", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
 	keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-	keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+	keymap(bufnr, "n", "gz", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
 	keymap(bufnr, "n", "gp", "<cmd>lua require 'user.lsp.util'.peek_definition()<CR>", opts)
 	keymap(bufnr, "n", "<leader>lf", "<cmd>lua vim.lsp.buf.format{ async = true }<cr>", opts)
 	keymap(bufnr, "n", "<leader>li", "<cmd>LspInfo<cr>", opts)
@@ -81,7 +81,7 @@ M.on_attach = function(client, bufnr)
 	-- 	client.server_capabilities.documentFormattingProvider = true
 	-- end
 
-	lsp_keymaps(bufnr)
+	M.lsp_keymaps(bufnr)
 	local status_ok, illuminate = pcall(require, "illuminate")
 	if not status_ok then
 		return
