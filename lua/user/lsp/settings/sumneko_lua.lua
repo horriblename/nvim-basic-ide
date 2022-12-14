77983f132bf7111908499830e0740b3e142c209b
Ching Pei Yang <badnam3o.0@gmail.com>
2022-12-14 01:16:07 +0100

    fix: sumenko_lua settings

--- lua/user/lsp/settings/sumneko_lua.lua
+++ lua/user/lsp/settings/sumneko_lua.lua
@@ -1,16 +1,22 @@
 return {
   settings = {
     Lua = {
+      runtime = {
+        version = "LuaJIT",
+        special = {
+          reload = "require",
+        },
+      },
       diagnostics = {
         globals = { "vim" },
       },
       workspace = {
         library = {
-          [vim.fn.expand "$VIMRUNTIME/lua"] = true,
-          [vim.fn.stdpath "config" .. "/lua"] = true,
+          [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+          [vim.fn.stdpath("config") .. "/lua"] = true,
+          [require("user.nvide-path"):get_nvide_config_dir() .. "/lua"] = true,
           -- these are not working:
-          [require 'user.nvide-path':get_nvide_config_dir() .. '/lua'] = true,
-          [require 'user.nvide-path':get_nvide_data_dir() .. '/site'] = true,
+          [require("user.nvide-path"):get_nvide_data_dir() .. "/site"] = true,
         },
       },
       telemetry = {
