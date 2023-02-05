8efaee156ee9ebd4f69832bd330d1dedbcc9de6e
Ching Pei Yang <badnam3o.0@gmail.com>
2023-02-05 16:46:17 +0100

    cleanup: remove redundant autocommand

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -2,23 +2,23 @@
 local user_augroup = vim.api.nvim_create_augroup("UserAutocmds", {})
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
-	group = user_augroup,
-	pattern = { "qf", "help", "man", "lspinfo", "spectre_panel" },
-	callback = function()
-		vim.cmd([[
+  group = user_augroup,
+  pattern = { "qf", "help", "man", "lspinfo", "spectre_panel" },
+  callback = function()
+    vim.cmd([[
       nnoremap <silent> <buffer> q :close<CR> 
       set nobuflisted 
     ]])
-	end,
+  end,
 })
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
-	group = user_augroup,
-	pattern = { "gitcommit", "markdown" },
-	callback = function()
-		vim.opt_local.wrap = true
-		vim.opt_local.spell = true
-	end,
+  group = user_augroup,
+  pattern = { "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = true
+  end,
 })
 
 vim.cmd("autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif")
@@ -38,51 +38,43 @@ vim.cmd("autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTre
 -- })
 
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
-	group = user_augroup,
-	callback = function()
-		vim.highlight.on_yank({ higroup = "Visual", timeout = 200 })
-	end,
+  group = user_augroup,
+  callback = function()
+    vim.highlight.on_yank({ higroup = "Visual", timeout = 200 })
+  end,
 })
 
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-	group = user_augroup,
-	pattern = { "*.java" },
-	callback = function()
-		vim.lsp.codelens.refresh()
-	end,
+  group = user_augroup,
+  pattern = { "*.java" },
+  callback = function()
+    vim.lsp.codelens.refresh()
+  end,
 })
 
 vim.api.nvim_create_autocmd({ "VimEnter" }, {
-	group = user_augroup,
-	callback = function()
-		vim.cmd("hi link illuminatedWord LspReferenceText")
-	end,
+  group = user_augroup,
+  callback = function()
+    vim.cmd("hi link illuminatedWord LspReferenceText")
+  end,
 })
 
 vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
-	group = user_augroup,
-	callback = function()
-		local line_count = vim.api.nvim_buf_line_count(0)
-		if line_count >= 5000 then
-			vim.cmd("IlluminatePauseBuf")
-		end
-	end,
-})
-
-vim.api.nvim_create_autocmd({ "BufEnter" }, {
-	group = user_augroup,
-	pattern = { "*.png", "*.jpg", "*.svg" },
-	callback = function()
-		vim.cmd("call jobstart('nsxiv -N nsxiv-float ' .. expand('%')) | bd")
-	end,
+  group = user_augroup,
+  callback = function()
+    local line_count = vim.api.nvim_buf_line_count(0)
+    if line_count >= 5000 then
+      vim.cmd("IlluminatePauseBuf")
+    end
+  end,
 })
 
 -- auto format
 vim.api.nvim_create_autocmd({ "BufWrite" }, {
-	group = user_augroup,
-	callback = function()
-		if vim.g.autoformatting then
-			vim.lsp.buf.format()
-		end
-	end,
+  group = user_augroup,
+  callback = function()
+    if vim.g.autoformatting then
+      vim.lsp.buf.format()
+    end
+  end,
 })
