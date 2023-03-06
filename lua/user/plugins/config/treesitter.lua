85ace83215ae94d2265e2cfab47309e6bd9062d2
Ching Pei Yang <badnam3o.0@gmail.com>
2023-03-06 03:53:33 +0100

    add treesitter-rainbow config

--- lua/user/plugins/config/treesitter.lua
+++ lua/user/plugins/config/treesitter.lua
@@ -13,10 +13,10 @@ end
 configs.setup({
   ensure_installed = { "lua", "markdown", "markdown_inline", "bash", "python" }, -- put the language you want in this array
   -- ensure_installed = "all", -- one of "all" or a list of languages
-  ignore_install = { "" },                                                      -- List of parsers to ignore installing
-  sync_install = false,                                                         -- install languages synchronously (only applied to `ensure_installed`)
+  ignore_install = { "" },                                                       -- List of parsers to ignore installing
+  sync_install = false,                                                          -- install languages synchronously (only applied to `ensure_installed`)
   highlight = {
-    enable = true, -- false will disable the whole extension
+    enable = true,                                                               -- false will disable the whole extension
   },
   autopairs = {
     enable = true,
@@ -49,7 +49,7 @@ configs.setup({
       -- mapping query_strings to modes.
       selection_modes = {
         ["@parameter.outer"] = "v", -- charwise
-        ["@function.outer"] = "V", -- linewise
+        ["@function.outer"] = "V",  -- linewise
         ["@class.outer"] = "<c-v>", -- blockwise
       },
       -- Can also be a function which gets passed a table with the keys
@@ -107,7 +107,14 @@ configs.setup({
     },
   },
 })
-require("nvim-treesitter.configs").setup({})
+require("nvim-treesitter.configs").setup({
+  rainbow = {
+    enable = true,
+    -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+    extended_mode = true,  -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+    max_file_lines = 5000, -- Do not enable for files with more than n lines, int
+  },
+})
 local function conceal_as_devicon(match, _, bufnr, pred, metadata)
   if #pred == 2 then
     -- (#as_devicon! @capture)
